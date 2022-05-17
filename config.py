5709227dadad0146124402660e6578a969ab4dbb
LoiseMulwa <loisemulwa7@gmail.com>
2022-05-17 21:52:23 +0300

    ft:LInk database in config file

--- config.py
+++ config.py
@@ -1,43 +1,43 @@
-# import os
+import os
 
-# class Config:
+class Config:
 
-#     SECRET_KEY = 'SECRET_KEY'
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:loise123@localhost/dev'
-#     UPLOADED_PHOTOS_DEST ='app/static/photos'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-#     #  email configurations
-#     MAIL_SERVER = 'loisemulwa7@gmail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SECRET_KEY = 'SECRET_KEY'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://developer:loise2@localhost/flask4'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
+    #  email configurations
+    MAIL_SERVER = 'loisemulwa7@gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 
-# class ProdConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # or other relevant config var
-#     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-#         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-#     # rest of connection code using the connection string `uri`
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # or other relevant config var
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    # rest of connection code using the connection string `uri`
    
 
 
-# class TestConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:loise@123@localhost/dev'
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://developer:loise2@localhost/flask4'
 
-#     pass
+    pass
 
 
-# class DevConfig(Config):
-#     DEBUG = True
+class DevConfig(Config):
+    DEBUG = True
 
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig,
-# 'test':TestConfig
-# }
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
